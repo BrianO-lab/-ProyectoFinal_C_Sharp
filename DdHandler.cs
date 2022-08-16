2fae39903cdd492e74db0cd6eeeb95b69f47651d
Brian Ore <orebrian95@gmail.com>
2022-08-15 22:49:11 -0300

    agregar los archivos c# faltantes

---
+++ DdHandler.cs
@@ -0,0 +1,8 @@
+namespace ProyectoFinal
+
+{
+    public abstract class DbHandler
+    {
+        public const string ConnectionString = "Server=BRIAN-AORUS;Database=SistemaGestion;Trusted_Connection=true";
+    }
+}
---
+++ VentaHandler.cs
@@ -0,0 +1,42 @@
+using System.Data.SqlClient;
+
+namespace ProyectoFinal
+{
+    public class VentaHandler : DbHandler
+    {
+        public List<Venta> GetProductosVendidoo()
+        {
+            List<Venta> resultado = new List<Venta>();
+
+            using (SqlConnection sqlConnection = new SqlConnection(ConnectionString))
+            {
+                using (SqlCommand sqlCommand = new SqlCommand("SELECT * FROM Usuario", sqlConnection))
+                {
+                    sqlConnection.Open();
+
+                    using (SqlDataReader dataReader = sqlCommand.ExecuteReader())
+                    {
+                        if (dataReader.HasRows)
+                        {
+                            while (dataReader.Read())
+                            {
+                                Venta vent = new Venta();
+
+                                vent.Id = Convert.ToInt32(dataReader["Id"]);
+                                vent.NombreUsuario = dataReader["NombreUsuario"].ToString();
+                                vent.Nombre = dataReader["Nombre"].ToString();
+                                vent.Apellido = dataReader["Apellido"].ToString();
+                                vent.Contraseña = dataReader["Contraseña"].ToString();
+                                vent.Mail = dataReader["Mail"].ToString();
+
+                                resultado.Add(vent);
+                            }
+                        }
+                    }
+                    sqlConnection.Close();
+                }
+            }
+            return resultado;
+        }
+    }
+}
