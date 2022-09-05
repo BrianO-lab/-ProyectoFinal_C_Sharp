f8a5d61fe1e757664153c9f7f92245b7d29817a6
Brian Ore <orebrian95@gmail.com>
2022-09-05 20:38:09 -0300

    .

--- DdHandler.cs
+++ DdHandler.cs
@@ -5,6 +5,6 @@ namespace ProyectoFinal
 {
     public abstract class DbHandler
     {
-        public const string ConnectionString = "Server=tcp:BRIAN-AORUS;Database=SistemaGestion;Trusted_Connection=True";
+        public const string ConnectionString = "Server=BRIAN-AORUS;Database=SistemaGestion;Trusted_Connection=True";
     }
 }
